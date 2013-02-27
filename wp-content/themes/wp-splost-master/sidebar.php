8358f6fa6e736748562d7c23aa28c9201b736200
Alyson La <alyson@github.com>
2013-02-26 21:17:29 -0800

    changes to theme

--- wp-content/themes/wp-splost-master/sidebar.php
+++ wp-content/themes/wp-splost-master/sidebar.php
@@ -1,105 +1,105 @@
 <div id="sidebar">
-  <h5>OVERVIEW & REPORTS</h5>
+  <h5>Header 1</h5>
   <div class="menu">
-   <div class="button"><a href="/splost/overview/">SPLOST Overview</a></div>   
+   <div class="button"><a href="/splost/overview/">Finance Overview</a></div>   
    <div class="button"><a href="/splost/revenue/">Montly Revenue</a></div>
   </div>
   <div style="clear:both;"></div>
    
-  <h5>SPLOST CATEGORIES & FOCUS AREAS</h5>
+  <h5>Header 2</h5>
     <div class="menu">
     <ul class="menu_cat">
-      <li class="category ps"><a href="/public-safety/">Public Safety Category</a></li>
+      <li class="category ps"><a href="/public-safety/">Sub Header</a></li>
     </ul>
 
     <ul>
       <!-- class 'ps' styles color and 'first'/'second' gives it appropriate padding on row -->
       <!-- update your urls when you have set up the pages and know them -->
-      <li class="ps first"><a href="/public-safety/800-mhz/">800 MHz</a></li> 
-      <li class="ps second"><a href="/public-safety/public-safety-equipment/">Public Safety Equipment</a></li>
-      <li class="ps first"><a href="/public-safety/public-safety-other/">Public Safety Other</a></li>  
-      <li class="ps second"><a href="/public-safety/animal-welfare-center/">Animal Welfare Center</a></li>
-      <li class="ps first"><a href="/public-safety/juvenile-justice-center/">Juvenile Justice Center</a></li>
-      <li class="ps second"><a href="/public-safety/fire-stations/">Fire Stations</a></li>
-      <li class="ps first"><a href="/public-safety/sheriff-cars-and-other-equipment/">Sheriff Cars and Other Equipment</a></li>
-      <li class="ps first"><a href="/public-safety/courthouse-and-building-equipment/">Courthouse and Building Improvements</a></li>
+      <li class="ps first"><a href="/public-safety/800-mhz/">Link</a></li> 
+      <li class="ps second"><a href="/public-safety/public-safety-equipment/">Link</a></li>
+      <li class="ps first"><a href="/public-safety/public-safety-other/">Link</a></li>  
+      <li class="ps second"><a href="/public-safety/animal-welfare-center/">Link</a></li>
+      <li class="ps first"><a href="/public-safety/juvenile-justice-center/">Link</a></li>
+      <li class="ps second"><a href="/public-safety/fire-stations/">Link</a></li>
+      <li class="ps first"><a href="/public-safety/sheriff-cars-and-other-equipment/">Link</a></li>
+      <li class="ps first"><a href="/public-safety/courthouse-and-building-equipment/">Link</a></li>
      </ul>
 </div><!-- #menu END -->
      
 <div class="menu">
   <ul class="menu_cat">
-    <li class="category dr"><a href="/debt-retirement/">Debt Retirement Category</a></li>
+    <li class="category dr"><a href="/debt-retirement/">Sub Header</a></li>
   </ul>
 
   <ul>
-    <li class="dr first"><a href="/debt-retirement/macon-debt-retirement/">Macon Debt Retirement</a></li>
-    <li class="dr second"><a href="/debt-retirement/bibb-debt-retirement/">Bibb Debt Retirement</a></li>
-    <li class="dr first"><a href="/debt-retirement/bond-repayment/">Bond Repayment</a></li>
-    <li class="dr second"><a href="/debt-retirement/macon-gma-lease-pool/">Macon GMA Lease Pool</a></li>
-    <li class="dr first"><a href="/debt-retirement/bibb-gma-lease-pool/">Bibb GMA Lease Pool</a></li>
+    <li class="dr first"><a href="/debt-retirement/macon-debt-retirement/">Link</a></li>
+    <li class="dr second"><a href="/debt-retirement/bibb-debt-retirement/">Link</a></li>
+    <li class="dr first"><a href="/debt-retirement/bond-repayment/">Link</a></li>
+    <li class="dr second"><a href="/debt-retirement/macon-gma-lease-pool/">Link</a></li>
+    <li class="dr first"><a href="/debt-retirement/bibb-gma-lease-pool/">Link</a></li>
   </ul>
 </div><!-- #menu END -->
      
 <div class="menu">
   <ul class="menu_cat">
-    <li class="category ed"><a href="/economic-development/">Economic Development Category</a></li>
+    <li class="category ed"><a href="/economic-development/">Sub Header</a></li>
   </ul>
 
   <ul>
-    <li class="ed first"><a href="/economic-development/centreplex/">Centreplex</a></li>
-    <li class="ed second"><a href="/economic-development/downtown-corridor/">Downtown Corridor</a></li>
-    <li class="ed first"><a href="/economic-development/acquisition-for-brac/">Acquisition for BRAC</a></li>
-    <li class="ed first"><a href="/economic-development/acquisition-for-economic-development/">Acquisition for Economic Development</a></li>
+    <li class="ed first"><a href="/economic-development/centreplex/">Link</a></li>
+    <li class="ed second"><a href="/economic-development/downtown-corridor/">Link</a></li>
+    <li class="ed first"><a href="/economic-development/acquisition-for-brac/">Link</a></li>
+    <li class="ed first"><a href="/economic-development/acquisition-for-economic-development/">Link</a></li>
   </ul>
 </div><!-- #menu END -->
 
 <div class="menu">
   <ul class="menu_cat">
-    <li class="category if"><a href="/infrastructure/">Infrastructure</a></li>
+    <li class="category if"><a href="/infrastructure/">Sub Header</a></li>
   </ul>
 
   <ul>
-   <li class="if first"><a href="/public-safety/bibb-storm-drainage/">Bibb Storm Drainage</a></li>
-   <li class="if second"><a href="/public-safety/bibb-street-maintenance/">Bibb Street Maintenance</a></li>
-   <li class="if first"><a href="/public-safety/macon-storm-drainage/">Macon Storm Drainage</a></li>
-   <li class="if first"><a href="/public-safety/macon-street-maintenance/">Macon Street Maintenance</a></li>
+   <li class="if first"><a href="/public-safety/bibb-storm-drainage/">Link</a></li>
+   <li class="if second"><a href="/public-safety/bibb-street-maintenance/">Link</a></li>
+   <li class="if first"><a href="/public-safety/macon-storm-drainage/">Link</a></li>
+   <li class="if first"><a href="/public-safety/macon-street-maintenance/">Link</a></li>
   </ul>
 </div><!-- #menu END -->
      
 <div class="menu">
   <ul class="menu_cat">
-    <li class="category ra"><a href="/rec-and-cultural-arts/">Rec & Cultural Arts Category</a></li>
+    <li class="category ra"><a href="/rec-and-cultural-arts/">Sub Header</a></li>
   </ul>
 
   <ul>
-    <li class="ra first"><a href="/rec-and-cultural-arts/tubman-museum/">Tubman Museum</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/bowden-golf/">Bowden Golf</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/rose-hill-cemetery/">Rose Hill Cemetery</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/ft-hawkins/">Ft. Hawkins</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/purchase-rec-equipment/">Purchase Rec Equipment</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/purchase-rec-land/">Purchase Rec Land</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/new-parks-and-rec-office-and-senior-center/">New Parks and Rec Office and Senior Center</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/tatnall-square-park/">Tatnall Square Park</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/lizella/">Lizella</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/sub-south-complex/">Sub South Complex</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/henry-burns-park/">Henry Burns Park</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/mattie-jones-hubbard-park/">Mattie Jones Hubbard Park</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/filmore-thomas-center/">Filmore Thomas Center</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/east-macon-center">East Macon Center</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/rosa-jackson-center/">Rosa Jackson Center</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/north-macon-center/">North Macon Center</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/freedom-center/">Freedom Center</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/frank-johnson-center/">Frank Johnson Center</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/bloomfield-center/">Bloomfield Center</a></li>
-    <li class="ra second"><a href="/rec-and-cultural-arts/john-drew-tennis-center/">John Drew Tennis Center</a></li>
-    <li class="ra first"><a href="/rec-and-cultural-arts/central-city-park/">Central City Park</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/tubman-museum/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/bowden-golf/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/rose-hill-cemetery/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/ft-hawkins/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/purchase-rec-equipment/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/purchase-rec-land/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/new-parks-and-rec-office-and-senior-center/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/tatnall-square-park/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/lizella/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/sub-south-complex/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/henry-burns-park/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/mattie-jones-hubbard-park/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/filmore-thomas-center/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/east-macon-center">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/rosa-jackson-center/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/north-macon-center/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/freedom-center/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/frank-johnson-center/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/bloomfield-center/">Link</a></li>
+    <li class="ra second"><a href="/rec-and-cultural-arts/john-drew-tennis-center/">Link</a></li>
+    <li class="ra first"><a href="/rec-and-cultural-arts/central-city-park/">Link</a></li>
   </ul>
 </div><!-- #menu END -->
 
 <div style="clear:both;"></div>
-  <h5>PRESS & DOCUMENTS</h5>
+  <h5>Header 3</h5>
   <div class="menu">
-    <div class="button"><a href="/tag/document/">SPLOST Documents</a></div>    
+    <div class="button"><a href="/tag/document/">Documents</a></div>    
   </div><!-- #menu END -->
 
   <div class="searcher">
